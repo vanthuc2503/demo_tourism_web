38744f4c3907ebcc2d08470f71bf185d466e13ea
vanthuc2503 <thucpkbn@gmail.com>
2025-10-20 21:35:03 +0700

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/blue-hanoi-style/" : "/",
+  base: mode === "production" ? "/demo_tourism_web/" : "/",
   server: {
     host: "::",
     port: 8080,
